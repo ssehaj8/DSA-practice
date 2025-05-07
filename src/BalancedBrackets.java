6647593602dd6386a1aa3c84159697ed021c103f
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-05-07 23:56:44 +0530

    [Sehajpreet] Add: Balanced Brackets Problem

--- src/BalancedBrackets.java
+++ src/BalancedBrackets.java
@@ -10,7 +10,7 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class Result {
+class Result3 {
 
     /*
      * Complete the 'isBalanced' function below.
