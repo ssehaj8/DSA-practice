128183513f752f635b50173a4c85c1fe96422345
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-05-13 09:36:46 +0530

    [Sehajpreet] Add:program that helps in printing stack heights

--- equalstacks.java
+++ equalstacks.java
@@ -65,7 +65,7 @@ class Result {
 
 }
 
-public class EqualStacks {
+public class equalstacks {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
