a736df3c108dfe9da87298275494fa2cdeea3b41
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-05-13 09:35:38 +0530

    [Sehajpreet] Add:program that helps in printing stack heights

---
+++ equalstacks.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'equalStacks' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h1
+     *  2. INTEGER_ARRAY h2
+     *  3. INTEGER_ARRAY h3
+     */
+
+    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
+        // Write your code here
+        Stack<Integer> s1 = buildStack(h1);
+        Stack<Integer> s2 = buildStack(h2);
+        Stack<Integer> s3 = buildStack(h3);
+
+        while (!(s1.isEmpty() || s2.isEmpty() || s3.isEmpty())) {
+            int top1 = s1.peek();
+            int top2 = s2.peek();
+            int top3 = s3.peek();
+
+            // If all three are equal, return the height
+            if (top1 == top2 && top2 == top3) {
+                return top1;
+            }
+
+            // Remove the topmost  stack's top element
+            if (top1 >= top2 && top1 >= top3) {
+                s1.pop();
+            } else if (top2 >= top1 && top2 >= top3) {
+                s2.pop();
+            } else {
+                s3.pop();
+            }
+        }
+
+        return 0; // If all stacks become empty
+    }
+
+
+
+    public static Stack<Integer> buildStack(List<Integer> heights){
+        Stack<Integer> stack = new Stack<>();
+        int totalHeight = 0;
+        for(int i = heights.size()-1; i>=0; i--){
+            totalHeight += heights.get(i);
+            stack.push(totalHeight);
+        }
+        return stack;
+    }
+
+}
+
+public class EqualStacks {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int n2 = Integer.parseInt(firstMultipleInput[1]);
+
+        int n3 = Integer.parseInt(firstMultipleInput[2]);
+
+        List<Integer> h1 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> h2 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> h3 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.equalStacks(h1, h2, h3);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
