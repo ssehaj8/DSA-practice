9af2e9ae3514886b9c775321fd2fb11d4b2a4439
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-05-13 10:07:10 +0530

    [Sehajpreet] Add: Level order Traversal

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    public static void levelOrder(Node root) {
+        if (root == null) return;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            System.out.print(current.data + " ");
+
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
+}
